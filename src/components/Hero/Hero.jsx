845ec82f43f66bf7efe6109c6f81025ea771da21
juaneugenio <leuviah77@gmail.com>
2025-04-05 10:55:23 +0200

    estilizando hero en md y lg screens

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,17 +10,17 @@ const Hero = () => {
   };
 
   return (
-    <div className="bg-gradient-to-bl from-emerald-800 to-emerald-200 px-6 py-10 md:px-8 md:py-32 md:h-[60vh]">
+    <div className="bg-gradient-to-bl from-emerald-800 to-emerald-200 px-4 py-10 md:px-6 md:h-[50vh] lg:h-[75vh]">
       <div className="relative w-full max-w-[1200px] mx-auto flex flex-col md:flex-row items-center h-full border-2">
         {/* Text Content */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8 }}
-          className="mb-5 md:w-[46%] md:px-8 md:absolute md:left-0 md:top-13 z-20 border-2"
+          className="mb-5 md:w-[450px] md:absolute md:left-0 md:top-13 z-20 border-2"
         >
           <motion.h1 
-            className="text-5xl md:text-7xl font-bold mb-6"
+            className="text-5xl md:text-6xl md:pt-0 font-bold mb-6"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.2, duration: 0.8 }}
@@ -57,7 +57,7 @@ const Hero = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
-          className="flex justify-center items-center md:w-auto md:absolute md:right-0 md:bottom-0 border-2 border-amber-300"
+          className="flex justify-center items-center md:max-w-[500px] md:absolute md:right-0 md:bottom-25 lg:max-w-3/4 lg:bottom-25 lg:right-5 border-2 border-amber-300"
         >
           <img 
             src="/images/hero-car.webp" 
