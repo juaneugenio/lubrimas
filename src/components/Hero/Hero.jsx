ad69545a95a40412e503103edbe910bbb03c5087
juaneugenio <leuviah77@gmail.com>
2025-04-05 16:59:00 +0200

    estilos del hero terminados. falta la funcionalidad del call-to-action.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,17 +10,17 @@ const Hero = () => {
   };
 
   return (
-    <div className="bg-gradient-to-bl from-emerald-800 to-emerald-200 px-4 py-10 md:px-6 md:h-[50vh] lg:h-[75vh]">
-      <div className="relative w-full max-w-[1200px] mx-auto flex flex-col md:flex-row items-center h-full border-2">
+    <div className="bg-gradient-to-br from-gray-400 via-gray-100 to-blue-200 px-4 py-10 md:px-6 md:h-[50vh] lg:h-[70vh]">
+      <div className="relative w-full max-w-[1200px] mx-auto flex flex-col md:flex-row items-center h-full">
         {/* Text Content */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8 }}
-          className="mb-5 md:w-[450px] md:absolute md:left-0 md:top-13 z-20 border-2"
+          className="mb-5 md:w-[450px] md:absolute md:left-0 md:top-13 lg:w-[550px] lg:top-8 z-20"
         >
           <motion.h1 
-            className="text-5xl md:text-6xl md:pt-0 font-bold mb-6"
+            className="text-5xl font-bold mb-6 md:text-6xl md:pt-0 lg:text-7xl"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.2, duration: 0.8 }}
@@ -29,7 +29,7 @@ const Hero = () => {
           </motion.h1>
           
           <motion.p
-            className="text-gray-600 text-lg mb-8"
+            className="text-gray-500 text-lg mb-8 md:text-xl md:w-78"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.4, duration: 0.8 }}
@@ -44,10 +44,10 @@ const Hero = () => {
           >
             <button
               onClick={handleServicesClick}
-              className="flex items-center font-bold text-primary hover:text-[#2a4a8a] transition-colors"
+              className="flex items-center font-bold p-0 bg-transparent border-none text-primary hover:text-blue-600 cursor-pointer transition-colors text-lg lg:text-2xl"
             >
               Escoge tu servicio
-              <ArrowRight className="ml-2" size={20} />
+              <ArrowRight className="ml-2 text-primary hover:text-orange-600 cursor-pointer" size={20} />
             </button>
           </motion.div>
         </motion.div>
@@ -57,7 +57,7 @@ const Hero = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
-          className="flex justify-center items-center md:max-w-[500px] md:absolute md:right-0 md:bottom-25 lg:max-w-3/4 lg:bottom-25 lg:right-5 border-2 border-amber-300"
+          className="flex justify-center items-center md:max-w-[500px] md:absolute md:right-0 md:bottom-10 lg:max-w-3/4 lg:bottom-25 lg:right-5"
         >
           <img 
             src="/images/hero-car.webp" 
